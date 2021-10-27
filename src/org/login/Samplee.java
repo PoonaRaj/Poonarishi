df4eb04049dd3f0e1a27901400768dd1754d47c5
PoonaRajkumar <poonarajkumar02@gmail.com>
2021-10-27 14:32:44 +0530

    First Attempt

---
+++ src/org/login/Samplee.java
@@ -0,0 +1,18 @@
+package org.login;
+
+public class Samplee {
+public static void main(String[] args) {
+	int [] a=new int [7];
+	a[0]=5;
+	a[1]=10;
+	a[2]=15;
+	a[3]=20;
+	a[4]=25;
+	a[5]=30;
+	a[6]=35;
+	
+		System.out.println(a[i]);
+		
+	}
+
+}
